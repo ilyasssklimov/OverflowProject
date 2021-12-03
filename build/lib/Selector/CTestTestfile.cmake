71c008ac1d7b5fea3b3ac11b1480770418df8bb7
Vdmch <vdmchu@gmail.com>
2021-12-03 11:54:56 +0000

    gitignore tweaks

--- build/lib/Selector/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justcrab/vscode/OverflowProject/lib/Selector
-# Build directory: /home/justcrab/vscode/OverflowProject/build/lib/Selector
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
