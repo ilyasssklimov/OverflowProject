170cb7c591d7855f83b20fb297232bf52af93544
Vdmch <vdmchu@gmail.com>
2021-12-03 11:54:56 +0000

    gitignore tweaks

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justcrab/vscode/OverflowProject
-# Build directory: /home/justcrab/vscode/OverflowProject/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/Selector")
--- build/lib/Selector/CMakeFiles/ParseJson.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libParseJson.a"
-)
