e3fee96adfebe3013e1a6228a11318537efd47be
Ilya <ilyasssklimov@gmail.com>
2021-12-08 23:30:38 +0300

    delete extra

--- lib/Selector/include/ParseJson.h
+++
@@ -1,7 +0,0 @@
-#include <vector>
-#include <string>
-
-class ParseJson{
-public:
-    std::vector<int> parse_json(std::string json);
-};
--- lib/Selector/include/Selector.h
+++
@@ -1,7 +0,0 @@
-#include <vector>
-#include "FlatAndTravelTime.h"
-
-class Selector{
-public:
-    virtual std::vector<FlatAndTravelTime> get_by_travel_time(int count, std::string dest) = 0;
-};
