762e1326fe7e01dbcebfa9d429737d13ba02e503
Vdmch <vdmchu@gmail.com>
2021-12-08 23:48:00 +0000

    datalib implemented

--- lib/Selector/include/FlatAndTravelTime.h
+++ lib/Selector/include/FlatAndTravelTime.h
@@ -1,16 +1,22 @@
-#include "flat.h"
+#ifndef _FLATANDTRAVELTIME_H__
+#define _FLATANDTRAVELTIME_H__
+
+#include "Selector.h"
 
 
 class FlatAndTravelTime{
 public:
-    FlatAndTravelTime(Flat *a_flat, int a_time) {
+    FlatAndTravelTime() = delete;
+    FlatAndTravelTime(Flat* a_flat, int a_time) {
         flat = a_flat;
         travel_time = a_time;
-};
+    }
 
-    int get_travel_time() const { return travel_time; };
-    Flat* get_flat() const { return flat; };
+    int get_travel_time() const { return travel_time; }
+    Flat* get_flat() const { return flat; }
 private:
-    Flat *flat;
+    Flat* flat;
     int travel_time;
 };
+
+#endif
--- lib/Selector/src/ParseJson.cpp
+++ lib/Selector/src/ParseJson.cpp
@@ -5,28 +5,25 @@
 #include <iostream>
 #include "ParseJson.h"
 
-
 std::vector<int> ParseJson::parse_json(std::string json){
     rapidjson::Document d;
 
     d.Parse(json.c_str());
-	int size = 0;
+	size_t size = 0;
 	rapidjson::Value elemenst;
-	try{
-		elemenst = d["rows"];
-		size = elemenst.Size();
-	}
-	catch(...){
-		std::cout << "Wrong JSON\n";
-	}
+	
+	if(!d.HasMember("rows")) return {};
+	elemenst = d["rows"];
+	size = elemenst.Size();
+		
 	std::vector<int> result;
-	try{
-		for(int i = 0; i < size; i++){
-			result.push_back(elemenst[i]["elements"][0]["duration"]["value"].GetInt());
-		}
-	}
-	catch(...){
-		std::cout << "Wrong Index\n";
+	for(size_t i = 0; i < size; i++){
+		if(elemenst.Size() <= i) return {};
+		if(!elemenst[i].HasMember("elements")) return {};
+		if(!elemenst[i]["elements"].IsArray()) return {};
+		if(!elemenst[i]["elements"][0].HasMember("duration")) return {};
+		if(!elemenst[i]["elements"][0]["duration"].HasMember("value")) return {};
+		result.push_back(elemenst[i]["elements"][0]["duration"]["value"].GetInt());
 	}
     return result;
 }
