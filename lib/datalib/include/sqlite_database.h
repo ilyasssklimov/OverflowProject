5d6eefb83b7fcd266b884b35cccd5bd92631c747
Роман <jellybe@yandex.ru>
2021-12-09 16:54:11 +0300

    Обновлен datalib.

--- lib/datalib/include/sqlite_database.h
+++ lib/datalib/include/sqlite_database.h
@@ -13,10 +13,16 @@
 class SQLiteDataBase: public IDataBase {
 public:
     explicit SQLiteDataBase(const std::string& db_name);
+    SQLiteDataBase(SQLiteDataBase &db) = default;
+    SQLiteDataBase &operator = (const SQLiteDataBase &db) = default;
+    SQLiteDataBase(SQLiteDataBase &&db) = delete;
+    SQLiteDataBase &operator = (const SQLiteDataBase &&db) = delete;
+
     ~SQLiteDataBase() override;
     const sqlite3& get_connection();
 
     void select_flats();
+    void delete_flats();
 
     bool add_flat(Flat &flat) override;
     bool add_flats(std::vector<Flat> &flats) override;
