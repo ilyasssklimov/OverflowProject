bbbd8377710ee5d43556c50558c24535bd5defbf
Ilya <ilyasssklimov@gmail.com>
2021-12-09 05:16:12 +0300

    one request to add flats && fix error with quotes && update constructors in db class

--- lib/datalib/src/sqlite_database.cpp
+++ lib/datalib/src/sqlite_database.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "sqlite_database.h"
 #include "db_exception.h"
 
@@ -67,34 +68,38 @@ void SQLiteDataBase::select_flats() {
     std::string sql_select_flat = "select * from flat";
     char *error_msg = nullptr;
     if (sqlite3_exec(connection, sql_select_flat.c_str(), select_callback, nullptr, &error_msg) != SQLITE_OK) {
-        fprintf(stderr, "Error while inserting: %s\n", error_msg);
+        std::cerr << "Error while selecting... " << error_msg << std::endl;
         sqlite3_free(error_msg);
     }
 }
 
 
-static void get_flat_insert(Flat &flat, std::string &sql_flat) {
-    sql_flat = "insert into flat (title, price, price_per_area, address, "
-               "description, station, property, floor, floor_max, photo) "
-               "values ('" + flat.get_title() + "', " + std::to_string(flat.get_price()) +
-               ", " + std::to_string(flat.get_price_per_area()) + ", '" +
-               flat.get_address() + "', '" + flat.get_description() + "', '" +
-               flat.get_station() + "', '" + flat.get_property() + "', " +
-               std::to_string(flat.get_floor()) + ", " + std::to_string(flat.get_floor_max()) +
-               ", '" + flat.get_photo() + "');";
+static std::string generate_string(Flat &flat) {
+    std::string description = flat.get_description();
+    std::replace(description.begin(), description.end(), '\'', '_');
+    flat.set_description(description);
+
+    std::string sql_flat = "\'" + flat.get_title() + "\', " + std::to_string(flat.get_price()) +
+                           ", " + std::to_string(flat.get_price_per_area()) + ", \'" +
+                           flat.get_address() + "\', \'" + flat.get_description() + "\', \'" +
+                           flat.get_station() + "\', \'" + flat.get_property() + "\', " +
+                           std::to_string(flat.get_floor()) + ", " + std::to_string(flat.get_floor_max()) +
+                           ", \'" + flat.get_photo() + "\'";
+    return sql_flat;
 }
 
 
 bool SQLiteDataBase::add_flat(Flat &flat) {
-    std::string sql_insert_flat;
-    get_flat_insert(flat, sql_insert_flat);
-    char *error_msg = nullptr;
+    std::string sql_flat = "insert into flat (title, price, price_per_area, address, "
+                           "description, station, property, floor, floor_max, photo) "
+                           "values (" + generate_string(flat) + ");";
 
-    if (sqlite3_exec(connection, sql_insert_flat.c_str(), nullptr, nullptr, &error_msg) == SQLITE_OK) {
+    char *error_msg = nullptr;
+    if (sqlite3_exec(connection, sql_flat.c_str(), nullptr, nullptr, &error_msg) == SQLITE_OK) {
         return true;
     }
     else {
-        fprintf(stderr, "Error while inserting: %s\n", error_msg);
+        std::cerr << "Error while inserting... " << error_msg << std::endl;
         sqlite3_free(error_msg);
         return false;
     }
@@ -102,12 +107,25 @@ bool SQLiteDataBase::add_flat(Flat &flat) {
 
 
 bool SQLiteDataBase::add_flats(std::vector<Flat> &flats) {
-    for (auto flat: flats) {
-        if (!add_flat(flat))
-            return false;
-    }
+    std::string sql_flats = "insert into flat "
+                            "(title, price, price_per_area, address, "
+                            "description, station, property, floor, "
+                            "floor_max, photo) values";
 
-    return true;
+    for (auto& flat: flats)
+        sql_flats += "(" + generate_string(flat) + "),\n";
+    sql_flats.erase(sql_flats.length() - 2, 2);
+    sql_flats += ";";
+
+    char *error_msg = nullptr;
+    if (sqlite3_exec(connection, sql_flats.c_str(), nullptr, nullptr, &error_msg) == SQLITE_OK) {
+        return true;
+    }
+    else {
+        std::cerr << "Error while inserting... " << error_msg << std::endl;
+        sqlite3_free(error_msg);
+        return false;
+    }
 }
 
 
@@ -127,10 +145,11 @@ static int get_callback(void *flats, int argc, char **argv, char **column_name) 
 
 std::vector<Flat> SQLiteDataBase::get_all_flats() {
     std::string sql_select_flat = "select * from flat";
-    char *error_msg = nullptr;
     std::vector<Flat> flats;
+
+    char *error_msg = nullptr;
     if (sqlite3_exec(connection, sql_select_flat.c_str(), get_callback, &flats, &error_msg) != SQLITE_OK) {
-        fprintf(stderr, "Error while inserting: %s\n", error_msg);
+        std::cerr << "Error while inserting... " << error_msg << std::endl;
         sqlite3_free(error_msg);
         return {};
     }
@@ -140,12 +159,23 @@ std::vector<Flat> SQLiteDataBase::get_all_flats() {
 
 std::vector<Flat> SQLiteDataBase::get_random_flats(int num) {
     std::string sql_select_flat = "select * from flat order by random() limit " + std::to_string(num) + ";";
-    char *error_msg = nullptr;
     std::vector<Flat> flats;
+
+    char *error_msg = nullptr;
     if (sqlite3_exec(connection, sql_select_flat.c_str(), get_callback, &flats, &error_msg) != SQLITE_OK) {
-        fprintf(stderr, "Error while inserting: %s\n", error_msg);
+        std::cerr << "Error while inserting... " << error_msg << std::endl;
         sqlite3_free(error_msg);
         return {};
     }
     return flats;
+}
+
+
+void SQLiteDataBase::delete_flats() {
+    std::string sql_delete_flat = "delete from flat";
+    char *error_msg = nullptr;
+    if (sqlite3_exec(connection, sql_delete_flat.c_str(), nullptr, nullptr, &error_msg) != SQLITE_OK) {
+        std::cerr << "Error while deleting... " << error_msg << std::endl;
+        sqlite3_free(error_msg);
+    }
 }
